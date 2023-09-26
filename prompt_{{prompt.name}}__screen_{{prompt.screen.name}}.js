aabc9bc7cb85a015e1c20febf1368c3990d1f0e2
sergioaguirreok <66813310+sergioaguirreok@users.noreply.github.com>
2023-09-26 13:44:30 -0300

    Add files via upload

---
+++ prompt_{{prompt.name}}__screen_{{prompt.screen.name}}.js
@@ -0,0 +1,18 @@
+const XPATH_LOGIN_ID__COUNTRYCODE_BUTTON = "//button[@value='pick-country-code']";
+const XPATH_LOGIN_ID__COUNTRYCODE_BUTTON_SPAN3 = "//button[@value='pick-country-code']/span[3]";
+
+var jsAvailableElement = getElementByXpath(XPATH_LOGIN_ID__JS_AVAILABLE);
+const jsAvailable = jsAvailableElement.getAttribute("value");
+const appName = "{{application.name}}";
+
+var pwdlessCountryCodeButton = getElementByXpath(XPATH_LOGIN_ID__COUNTRYCODE_BUTTON);
+
+if (pwdlessCountryCodeButton != null) {
+
+    //Disable the country code picker button
+    pwdlessCountryCodeButton.setAttribute("disabled", "")
+
+    //Remove > from the country code picker button
+    var pwdlessCountryButtonCodeSpan3 = getElementByXpath(XPATH_LOGIN_ID__COUNTRYCODE_BUTTON_SPAN3);
+    pwdlessCountryButtonCodeSpan3.parentElement.removeChild(pwdlessCountryButtonCodeSpan3)
+}
